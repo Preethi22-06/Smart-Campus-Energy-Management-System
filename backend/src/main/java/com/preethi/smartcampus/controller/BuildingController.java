760f32d15a4ac8a64e305a87610e96ce45568476
Preethi22-06 <preethip.ad24@bitsathy.ac.in>
2026-07-27 18:32:47 +0530

    Day 8: Completed CRUD operations for Building module

--- backend/src/main/java/com/preethi/smartcampus/controller/BuildingController.java
+++ backend/src/main/java/com/preethi/smartcampus/controller/BuildingController.java
@@ -1,5 +1,5 @@
 package com.preethi.smartcampus.controller;
-
+import java.util.Optional;
 import com.preethi.smartcampus.entity.Building;
 import com.preethi.smartcampus.service.BuildingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +18,21 @@ public class BuildingController {
     public List<Building> getAllBuildings() {
         return buildingService.getAllBuildings();
     }
+    @GetMapping("/{id}")
+public Optional<Building> getBuildingById(@PathVariable Long id) {
+    return buildingService.getBuildingById(id);
+}
 
     @PostMapping
     public Building addBuilding(@RequestBody Building building) {
         return buildingService.saveBuilding(building);
     }
+    @PutMapping("/{id}")
+public Building updateBuilding(@PathVariable Long id, @RequestBody Building building) {
+    return buildingService.updateBuilding(id, building);
+}
+@DeleteMapping("/{id}")
+public void deleteBuilding(@PathVariable Long id) {
+    buildingService.deleteBuilding(id);
+}
 }
--- backend/src/main/java/com/preethi/smartcampus/service/BuildingService.java
+++ backend/src/main/java/com/preethi/smartcampus/service/BuildingService.java
@@ -1,5 +1,5 @@
 package com.preethi.smartcampus.service;
-
+import java.util.Optional;
 import com.preethi.smartcampus.entity.Building;
 import com.preethi.smartcampus.repository.BuildingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +9,7 @@ import java.util.List;
 
 @Service
 public class BuildingService {
+    
 
     @Autowired
     private BuildingRepository buildingRepository;
@@ -16,8 +17,18 @@ public class BuildingService {
     public List<Building> getAllBuildings() {
         return buildingRepository.findAll();
     }
+     public Optional<Building> getBuildingById(Long id) {
+    return buildingRepository.findById(id);
+}
 
     public Building saveBuilding(Building building) {
         return buildingRepository.save(building);
     }
+    public Building updateBuilding(Long id, Building updatedBuilding) {
+    updatedBuilding.setId(id);
+    return buildingRepository.save(updatedBuilding);
+}
+public void deleteBuilding(Long id) {
+    buildingRepository.deleteById(id);
+}
 }
