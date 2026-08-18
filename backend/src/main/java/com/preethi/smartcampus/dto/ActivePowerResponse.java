9c53465315ec64d362064061186e0ee3333f5a82
Preethi22-06 <preethi.padmanaban06@gmail.com>
2026-08-18 18:25:16 +0530

    Day 33: Improve active power API response

--- backend/src/main/java/com/preethi/smartcampus/dto/ActivePowerResponse.java
+++ backend/src/main/java/com/preethi/smartcampus/dto/ActivePowerResponse.java
@@ -3,12 +3,18 @@ package com.preethi.smartcampus.dto;
 public class ActivePowerResponse {
 
     private double activePower;
+    private String unit;
 
     public ActivePowerResponse(double activePower) {
         this.activePower = activePower;
+        this.unit = "W";
     }
 
     public double getActivePower() {
         return activePower;
+    }
+
+    public String getUnit() {
+        return unit;
     }
 }
